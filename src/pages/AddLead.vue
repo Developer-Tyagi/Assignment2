508eb97fa813ec0d627d66384759774d4d2583ba
Soumya Vikram <soumya.vicky@gmail.com>
2020-10-08 12:13:28 +0530

    Fixed format and url to kebab case

--- src/pages/AddLead.vue
+++ src/pages/AddLead.vue
@@ -1,60 +1,56 @@
 <template>
-    <q-page>
+  <q-page>
     <h5>Add New Lead</h5>
-     
-  <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
-    >
+
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
       <q-toggle v-model="isNewLead" label="Is this a new lead?" left-label color="orange"></q-toggle>
       <div v-if="!isNewLead">
-          <q-separator></q-separator>
-   <br><p>If client already exists, select from list below</p>
-   <q-select v-model="clientSelected" :options="clientsList" label="Select existing client"></q-select>
-    </div>
+        <q-separator></q-separator>
+        <br />
+        <p>If client already exists, select from list below</p>
+        <q-select v-model="clientSelected" :options="clientsList" label="Select existing client"></q-select>
+      </div>
       <div>
         <q-btn label="Back" type="reset" color="primary" flat class="q-ml-sm"></q-btn>
         <q-btn label="Next" type="submit" color="primary"></q-btn>
       </div>
     </q-form>
-    </q-page>   
+  </q-page>
 </template>
 <script>
 export default {
-methods: {
-    onSubmit () {
+  methods: {
+    onSubmit() {
       console.log(this.clientSelected.label);
-      this.$router.push('/addLeadDetails');
+      this.$router.push("/add-lead-details");
     },
-    onReset () {
-      this.$router.push('/leads');
+    onReset() {
+      this.$router.push("/leads");
     }
- },
- data () {
+  },
+  data() {
     return {
-      clientSelected : '',
-      isNewLead : false, 
+      clientSelected: "",
+      isNewLead: false,
       // Hardcoding this as still api for this in not avialable.
       clientsList: [
         {
-          label: 'James',
-          value: 'James'
+          label: "James",
+          value: "James"
         },
         {
-          label: 'Jane',
-          value: 'Jane'
+          label: "Jane",
+          value: "Jane"
         },
         ,
         {
-          label: 'Peter',
-          value: 'Peter'
+          label: "Peter",
+          value: "Peter"
         }
-      ],
-    }
- }
-}
+      ]
+    };
+  }
+};
 </script>
 <style>
-
 </style>
