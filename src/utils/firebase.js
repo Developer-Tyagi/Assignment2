3d93034e15c64bde3964caf99c3bc6f34c00ff8d
sonali <sonalisaxena9755@gmail.com>
2021-02-04 14:35:35 +0530

    env vars added

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAEJc2JbsAbjTtUDBs5xZugsDiZYIb98AY',
-  authDomain: 'pre-prod-286801.firebaseapp.com',
-  databaseURL: 'https://pre-prod-286801.firebaseio.com',
-  projectId: 'pre-prod-286801',
-  storageBucket: 'pre-prod-286801.appspot.com',
-  messagingSenderId: '452299913095',
-  appId: '1:452299913095:web:f6ed43665baf89af5e8522',
-  measurementId: 'G-4H5J4F5KRW'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 // Initialize Firebase
 
