61df679899dd43054d71063ccbff65439898608b
Animesh <animesh@cilalabs.com>
2021-02-05 13:56:03 +0530

    changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,35 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-import leads from './leads'
-import vendors from './vendors'
-import common from './common'
-import clients from './clients'
-import user from './user'
-import settings from './settings'
-Vue.use(Vuex)
+import leads from './leads';
+import vendors from './vendors';
+import common from './common';
+import clients from './clients';
+import user from './user';
+import settings from './settings';
+Vue.use(Vuex);
 
 /*
  * If not building with SSR mode, you can
  * directly export the Store instantiation
  */
 
-export default function( /* { ssrContext } */ ) {
-    const Store = new Vuex.Store({
-        modules: {
-            leads,
-            common,
-            user,
-            vendors,
-            clients,
-            settings
-        },
+export default function(/* { ssrContext } */) {
+  const Store = new Vuex.Store({
+    modules: {
+      leads,
+      common,
+      user,
+      vendors,
+      clients,
+      settings,
+      admin
+    }
 
-        // enable strict mode (adds overhead!)
-        // for dev mode only
-        // strict: process.env.DEV
-    })
+    // enable strict mode (adds overhead!)
+    // for dev mode only
+    // strict: process.env.DEV
+  });
 
-    return Store
+  return Store;
 }
