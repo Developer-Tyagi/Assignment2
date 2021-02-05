1c5c9f6c4654c40d518abf425ec17024c801f949
sonali <sonalisaxena9755@gmail.com>
2021-02-05 16:07:57 +0530

    changes in firebase

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -5,22 +5,23 @@ import firebaseAuthorization from '@utils/firebase';
 export async function userLogin({ commit, dispatch }, formData) {
   const { data } = formData;
   dispatch('setLoading', true);
-  try {
-    const res = await firebaseAuthorization.signInWithEmailAndPassword(
-      data.attributes.email,
-      data.attributes.password
-    );
-    const token = res.user.getIdToken();
-    if (token) {
-      setToken(data.attributes.token);
+  firebaseAuthorization
+    .signInWithEmailAndPassword(data.attributes.email, data.attributes.password)
+    .then(res => {
+      // Signed in
       dispatch('setLoading', false);
-      this.$router.push('/dashboard');
-    }
-  } catch (err) {
-    dispatch('setLoading', false);
-    dispatch('setNotification', {
-      type: 'negative',
-      message: 'Login failed. Please use correct email and password.'
+      res.user.getIdToken().then(token => {
+        setToken(token);
+        this.$router.push('/dashboard');
+      });
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      dispatch('setLoading', false);
+      dispatch('setNotification', {
+        type: 'negative',
+        message: 'Login failed. Please use correct email and password.'
+      });
     });
-  }
 }
