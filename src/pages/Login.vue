e34500128e80b010200152ad1ee314f9a6a9b070
Animesh <animesh@cilalabs.com>
2021-02-05 13:16:54 +0530

    changes

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -103,8 +103,6 @@ export default {
       };
       if (this.login.email && this.login.password) {
         this.userLogin(loginData);
-      } else {
-        this.$router.push('/account');
       }
     }
   },
