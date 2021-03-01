113da336cd3b4fcf5d63582ce96091829d7bcfdd
Animesh <animesh@cilalabs.com>
2021-03-02 04:32:47 +0530

    remove console log

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -16,7 +16,6 @@ export function validateUrl(url) {
 }
 
 export function validateDate(val) {
-  console.log('bbye');
   const regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
   if (regex.test(val)) {
     return true;
