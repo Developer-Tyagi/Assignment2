84e7b0e9f6d378a79234965549c2dd7840fb09c9
Animesh <animesh@cilalabs.com>
2021-03-05 15:14:44 +0530

    changes

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,27 +5,33 @@ const currentUser = 'current_user';
 const tokenSettings = { domain: process.env.DOMAIN || 'localhost' };
 
 export function setToken(token) {
-  Cookies.set(tokenName, token, tokenSettings);
+  // Cookies.set(tokenName, token, tokenSettings);
+  localStorage.setItem(tokenName, token);
 }
 
 export function removeToken() {
-  Cookies.remove(tokenName, tokenSettings);
+  // Cookies.remove(tokenName, tokenSettings);
+  localStorage.removeItem(tokenName);
 }
 
 export function getToken() {
-  return Cookies.get(tokenName);
+  // return Cookies.get(tokenName);
+  localStorage.getItem(tokenName);
 }
 
 export function setCurrentUser(user) {
-  Cookies.set(currentUser, user, tokenSettings);
+  // Cookies.set(currentUser, user, tokenSettings);
+  localStorage.setItem(currentUser, token);
 }
 
 export function removeCurrentUser() {
-  Cookies.remove(currentUser, tokenSettings);
+  // Cookies.remove(currentUser, tokenSettings);
+  localStorage.removeItem(currentUser);
 }
 
 export function getCurrentUser() {
-  return Cookies.get(currentUser);
+  // return Cookies.get(currentUser);
+  localStorage.getItem(currentUser);
 }
 
 export function guardMyroute(to, from, next) {
