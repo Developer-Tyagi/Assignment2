5ad0221fa96d8eac9bd3c44264f5b20b97c2c7e3
Animesh <animesh@cilalabs.com>
2021-03-15 16:26:16 +0530

    changes in style

--- src/layouts/MobileLayout.vue
+++ src/layouts/MobileLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout>
-    <q-page-container style="padding: 0; height: 100vh">
+    <q-page-container>
       <CustomHeader @backButton="$router.go(-1)" />
       <router-view />
     </q-page-container>
