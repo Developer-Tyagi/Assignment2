386703b41f19ed0123133c8ae8f99767cd113bbf
sonali <sonalisaxena9755@gmail.com>
2021-03-16 12:35:15 +0530

    clients list is clickable

---
+++ src/pages/ClientsDetails.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page style="padding-top: 0; height: 100vh">
+    <CustomHeader
+      @backButton="$router.push('/clients')"
+      :showAddButton="false"
+    />
+  </q-page>
+</template>
+<script>
+import CustomHeader from 'components/CustomHeader';
+export default {
+  data() {
+    return {};
+  },
+
+  components: {
+    CustomHeader
+  },
+  mounted() {},
+
+  methods: {}
+};
+</script>
+<style></style>
