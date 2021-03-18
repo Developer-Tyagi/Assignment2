c652bf45a0803f97d529e59d4c6e48537157eed5
Animesh <animesh@cilalabs.com>
2021-03-18 16:24:40 +0530

    auto-complete bug and changed layout for dashboard

---
+++ src/components/SubSideBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="full-height bg-grey-4" style="width: 20%">
+    <div>
+      <q-list separator>
+        <q-item
+          clickable
+          v-ripple
+          v-for="data in list"
+          :key="data.name"
+          @click="onListClick(data)"
+          class="q-px-none"
+        >
+          <q-item-section style="color: #666666">
+            <p class="title">{{ data.name }}</p>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SubSideBar',
+  props: {
+    list: {
+      type: Array,
+      required: true
+    }
+  },
+
+  methods: {
+    onListClick(data) {
+      this.$emit('onListClick', data);
+    }
+  }
+};
+</script>
