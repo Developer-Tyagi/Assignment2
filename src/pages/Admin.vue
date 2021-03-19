d33862c779972f38aa1e43b05e8f5a09e33e9e5f
himanshu.ranjan1 <himanshu.ranjan@cilalabs.com>
2021-03-19 16:30:28 +0530

    added-default-action-dialog-box

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -17,7 +17,7 @@
             <template class="">
               <q-tab-panels
                 class="q-ml-xl full-width  "
-                style="height: 800px;"
+                style="height : calc(100vh - 10px);"
                 v-model="tab"
                 animated
                 vertical
@@ -62,7 +62,7 @@
                     </div>
                   </q-card>
                 </q-tab-panel>
-                <q-tab-panel name="portalconfiguration">
+                <q-tab-panel name="actionItems">
                   <q-card class="q-pa-lg " style="" flat bordered>
                     <div class=" full-width">
                       <div class=" text-h5 text-bold">Claim Action Item</div>
@@ -73,11 +73,11 @@
                             color="primary"
                             label="+ Add Default Action "
                             class=" col-2"
-                            @click=""
+                            @click="addDefaultActionDialogBox = true"
                           />
                           <q-btn
                             color="primary"
-                            label="Refesh "
+                            label="Refresh "
                             class="q-mx-lg col-2"
                             @click=""
                           />
@@ -158,6 +158,226 @@
         </div>
       </div>
     </div>
+    <q-dialog v-model="addDefaultActionDialogBox" persistent>
+      <q-card class=" col-8 " style=" width:1200px;height: 800px">
+        <q-bar class=" row justify-between bg-primary " style="height:50px;">
+          <div class=" q-px-xs text-bold text-white">
+            Add Action Item
+          </div>
+          <q-btn dense flat icon="close" v-close-popup>
+            <q-tooltip>Close</q-tooltip>
+          </q-btn>
+        </q-bar>
+        <div class=" row q-mt-lg  full-width">
+          <div class="col-1-1 ">
+            <div class="q-ml-xl q-pa-md text-bold">Action Item :</div>
+          </div>
+          <div class="col-5">
+            <q-input class="q-mx-md" style="width: 300px" outlined />
+          </div>
+          <div class="col-1-1 ">
+            <div class=" q-pa-md text-bold ">Enabled :</div>
+          </div>
+          <div class="col-3 q-ml-lg">
+            <q-toggle v-model="enabled" left-label class="q-mt-xs " />
+            <q-badge color="primary" v-if="enabled == true">Enable</q-badge>
+            <q-badge class="q-mt-md" color="primary" v-else>Disable</q-badge>
+          </div>
+        </div>
+        <!-- //second -->
+        <div class=" row q-mt-md  full-width">
+          <div class="col-1-1 ">
+            <div class="q-ml-xl q-pa-md text-bold">Created When :</div>
+          </div>
+          <div class="col-3">
+            <q-select
+              outlined
+              options-dense
+              behavior="menu"
+              v-model="main.select"
+              :options="options"
+              label="Created when"
+            ></q-select>
+          </div>
+
+          <div
+            class="col-3 q-ml-lg"
+            v-if="main.select == 'nnj' || main.select == 'nknj'"
+          >
+            <q-select
+              class="  col-3"
+              outlined
+              options-dense
+              behavior="menu"
+              v-model="model"
+              :options="options"
+              label="Sub-option"
+            ></q-select>
+          </div>
+        </div>
+        <!-- 3 -->
+        <div class=" row q-mt-md  full-width">
+          <div class="col-1-1 ">
+            <div class="q-ml-xl q-pa-sm text-bold">Default Priority :</div>
+          </div>
+          <div class="col-5">
+            <q-toggle
+              v-model="defaultPriorityToggleButton"
+              left-label
+              class=" "
+            />
+            <q-badge color="primary" v-if="defaultPriorityToggleButton == true"
+              >Important</q-badge
+            >
+          </div>
+          <div class="col-1-1 ">
+            <div class=" q-pa-md">:</div>
+          </div>
+          <div class="col-3 q-ml-lg"></div>
+        </div>
+        <div class=" row q-mt-md  full-width">
+          <div class="col-1-1 row ">
+            <div class="q-ml-xl q-pa-md text-bold">Due Date :</div>
+            <div class="  q-ml-xl q-pt-md text-bold">Task will be Due</div>
+          </div>
+          <div class="col-2 q-mr-lg">
+            <q-input class="q-mx-lg" style="" outlined />
+          </div>
+
+          <div class="col-2">
+            <q-select
+              outlined
+              options-dense
+              behavior="menu"
+              :options="options"
+              label="Bussiness"
+            ></q-select>
+          </div>
+          <div class="col-1-1 ">
+            <div class=" q-pa-md text-bold ">days after it is created</div>
+          </div>
+        </div>
+        <div class=" row q-mt-md  full-width">
+          <div class="col-1-1 row ">
+            <div class="q-ml-xl q-pa-md text-bold">Assign To :</div>
+            <div class="  q-ml-xl q-pt-md text-bold">
+              <q-btn color="primary" size="10px" label="..Select" @click="" />
+            </div>
+          </div>
+        </div>
+        <div class=" row q-mt-md q-pl-xl  full-width">
+          <div class="col-5" style="margin-left:10%;">
+            <q-card class="q-pa-md text-bold" flat bordered>
+              Note : Actions are executed in the order specified
+            </q-card>
+          </div>
+        </div>
+        <div
+          v-for="(val, index) in value"
+          v-if="value.length > 0"
+          class="  row q-mt-md  full-width"
+        >
+          <div class="col-1-1 ">
+            <div class="q-ml-xl q-pa-md text-bold">Completion Action :</div>
+          </div>
+          <div class="col-3">
+            <q-select
+              outlined
+              options-dense
+              behavior="menu"
+              v-model="val.name"
+              :options="options"
+              label="Completion Action"
+            ></q-select>
+          </div>
+
+          <div
+            class="col-3 q-ml-lg"
+            v-if="val.name == 'nnj' || val.name == 'nknj'"
+          >
+            <q-select
+              class="  col-3"
+              outlined
+              options-dense
+              behavior="menu"
+              v-model="model"
+              :options="options"
+              label="Sub-option"
+            ></q-select>
+          </div>
+          <div class="  q-ml-xl q-pt-sm text-bold">
+            <q-icon size="md" color="primary" name="add" @click="Addlick" />
+          </div>
+          <div class="  q-ml-lg q-pt-sm text-bold">
+            <q-icon
+              size="md"
+              color="primary"
+              name="clear"
+              @click="removeCompletionAction(index)"
+            />
+          </div>
+        </div>
+        <div
+          v-for="(val, index) in value1"
+          v-if="value1.length > 0"
+          class="  row q-mt-md  full-width"
+        >
+          <div class="col-1-1 ">
+            <div class="q-ml-xl q-pa-md text-bold">OverDue Action:</div>
+          </div>
+          <div class="q-ml-lg col-3">
+            <q-select
+              outlined
+              options-dense
+              behavior="menu"
+              v-model="val.name"
+              :options="options"
+              label="OverDue"
+            ></q-select>
+          </div>
+
+          <div
+            class="col-3 q-ml-lg"
+            v-if="val.name == 'nnj' || val.name == 'nknj'"
+          >
+            <q-select
+              class="  col-3"
+              outlined
+              options-dense
+              behavior="menu"
+              v-model="model"
+              :options="options"
+              label="Sub-option"
+            ></q-select>
+          </div>
+          <div class="  q-ml-xl q-pt-sm text-bold">
+            <q-icon
+              size="md"
+              color="primary"
+              name="add"
+              @click="addMoreOverDue"
+            />
+          </div>
+          <div class="  q-ml-lg q-pt-sm text-bold">
+            <q-icon
+              size="md"
+              color="primary"
+              name="clear"
+              @click="RemoveOverDue(index)"
+            />
+          </div>
+        </div>
+
+        <div class="  q-mt-xl row justify-center">
+          <q-btn
+            color="primary"
+            label="Save"
+            class=" col-2 q-mt-xl"
+            @click="onClickSaveButton"
+          />
+        </div>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 <script>
@@ -168,6 +388,14 @@ export default {
   components: { SubSideBar },
   data() {
     return {
+      main: {
+        select: ''
+      },
+      enabled: false,
+      defaultPriorityToggleButton: false,
+      value: [{ name: '' }],
+      value1: [{ name: '' }],
+      addDefaultActionDialogBox: false,
       model: null,
       options: ['nknj', ',mmnjn', 'nnkmk', 'nnj', 'nkjn'],
       tab: '',
@@ -175,7 +403,7 @@ export default {
         { name: 'Account Summary', key: 'accountSummary' },
         { name: 'Company Setup', key: 'companySetup' },
         { name: 'Group Permission ', key: 'groupPermission' },
-        { name: ' PortalConfiguration', key: 'portalconfiguration' }
+        { name: ' Action items', key: 'actionItems' }
       ]
     };
   },
@@ -183,6 +411,25 @@ export default {
   methods: {
     setSelectedTab(e) {
       this.tab = e.key;
+    },
+    RemoveOverDue(val) {
+      this.value1.splice(val, 1);
+    },
+    onClickSaveButton() {
+      this.addDefaultActionDialogBox = false;
+    },
+    removeCompletionAction(val) {
+      this.value.splice(val, 1);
+    },
+    addMoreOverDue() {
+      this.value1.push({
+        name: ''
+      });
+    },
+    Addlick() {
+      this.value.push({
+        name: ''
+      });
     }
   },
 
@@ -190,7 +437,6 @@ export default {
     this.tab = 'accountSummary';
     if (getCurrentUser().attributes) {
       this.user = getCurrentUser().attributes;
-      console.log(this.user);
     }
   }
 };
