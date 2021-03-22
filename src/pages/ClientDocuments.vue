1ca978896397dcf30621a4276fb2d20c0d8783a5
Animesh <animesh@cilalabs.com>
2021-03-22 05:30:34 +0530

    changes

--- src/pages/ClientDocuments.vue
+++ src/pages/ClientDocuments.vue
@@ -1,27 +1,9 @@
 <template>
   <q-page>
-    <div class="height-without-header">
-      <ul>
-        <li>
-          Folder 1
-          <ul>
-            <li>SubFile 1</li>
-            <li>SubFile 2</li>
-            <li>SubFile 3</li>
-          </ul>
-        </li>
-        <li>
-          Folder 2
-          <ul>
-            <li>SubFile 4</li>
-            <li>SubFile 5</li>
-            <li>SubFile 6</li>
-          </ul>
-        </li>
-        <li>Main File 1</li>
-        <li>Main File 2</li>
-      </ul>
+    <div class="action-div">
+      <q-btn name="add" @click="addElement()">add</q-btn>
     </div>
+    <div id="fileSystem"></div>
   </q-page>
 </template>
 <script>
@@ -29,31 +11,66 @@ export default {
   name: 'ClientDocument',
   data() {
     return {
-      login: { password: '', email: '' }
+      fileSytem: [
+        {
+          name: 'docments',
+          type: 'folder',
+          children: [
+            {
+              name: 'ani',
+              file: 'asfdasfsafsf',
+              type: 'folder',
+              children: [
+                {
+                  name: 'ani',
+                  file: 'asfdasfsafsf',
+                  type: 'file'
+                }
+              ]
+            },
+            {
+              name: 'sonali',
+              file: 'asfdasfsafsf',
+              type: 'file'
+            },
+            {
+              name: 'ani',
+              file: 'asfdasfsafsf',
+              type: 'file'
+            }
+          ]
+        },
+        {
+          name: 'ani',
+          file: 'asfdasfsafsf',
+          type: 'folder',
+          children: []
+        }
+      ],
+      text: ''
     };
   },
   methods: {
-    onCardClick(route) {
-      this.$router.push(`/${route}`);
+    addFolder(id, text) {
+      document.getElementById(
+        id
+      ).innerHTML += `<button onclick="clicked()">${text} </button>`;
+    },
+
+    clicked() {
+      console.log('hi');
     }
+  },
+
+  mounted() {
+    let i = 0;
+    // this.fileSystem.forEach(element => {
+    //   if (element.type == 'folder') {
+    //     console.log(element.name);
+    //     this.addFolder('fileSystem', element.name);
+    //   }
+    // });
   }
 };
 </script>
-<style lang="scss" scoped>
-.container {
-  max-width: 100%;
-  width: 100%;
-  height: 110vh;
-}
-.my-card {
-  width: 100%;
-  max-width: 300px;
-  height: 100%;
-  max-height: 150px;
-}
-.my-font {
-  font-family: 'Lato';
-  font-size: 20px;
-  color: #333333;
-}
-</style>
+<style lang="scss" scoped></style>
