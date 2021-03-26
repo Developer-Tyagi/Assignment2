29e102bb5f70249e9c5ed0f1989d30b129332f60
sonali <sonalisaxena9755@gmail.com>
2021-03-26 12:21:45 +0530

    commented code removed

--- src/store/claims/mutations.js
+++ src/store/claims/mutations.js
@@ -1,9 +1,3 @@
 export function setClaims(state, claims) {
-  // console.log('int');
-  // state.claims = claims.map(claim => ({
-  //   ...claim.attributes,
-  //   id: claim.id
-  //   // name: claim.attributes.insuredInfo.primary['fname']
-  // }));
   state.claims = claims;
 }
