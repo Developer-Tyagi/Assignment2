012637fc3a1137b9a0680337a1ac7e95753630fe
Animesh <animesh@cilalabs.com>
2021-04-22 05:52:25 +0530

    changes

--- src/pages/AddClient.vue
+++ src/pages/AddClient.vue
@@ -1650,7 +1650,7 @@ export default {
   .step {
     display: flex;
     overflow-x: auto;
-    padding: 10px;
+    padding: 10px 10px 0 10px;
 
     .icon-div-selected {
       background: $primary;
