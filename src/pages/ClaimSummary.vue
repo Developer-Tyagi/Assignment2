1f85021727114d7f969fb48daf52f8f598197937
himanshu.ranjan1 <himanshu.ranjan@cilalabs.com>
2021-04-26 17:36:28 +0530

    editable-screen-of two sections claim summary and claim deadlines

--- src/pages/ClaimSummary.vue
+++ src/pages/ClaimSummary.vue
@@ -3,8 +3,17 @@
     <div class="listing-height">
       <ClaimDetail />
       <q-card class="q-ma-md q-pa-md  ">
-        <div class="row q-ml-xs">
-          <span class="text-bold col q-mt-xs"> Claim Summary</span>
+        <div class="row q-ml-xs justify-between">
+          <div class="text-bold  q-mt-xs">Claim Summary</div>
+          <div>
+            <q-icon
+              class="q-ml-xs"
+              name="edit"
+              size="xs"
+              color="primary"
+              @click="claimSummary = true"
+            />
+          </div>
         </div>
         <div class=" q-ml-xs">
           <div class="row q-mt-sm">
@@ -44,7 +53,7 @@
             </span>
           </div>
           <div class="row q-mt-sm">
-            <span class="heading-light "> Date of Contract </span>
+            <span class="heading-light col-4 "> Date of Contract </span>
             <span class="q-ml-md" v-if="getSelectedClaim.contractInfo">
               {{
                 getSelectedClaim.contractInfo.dateOfFirstContact
@@ -54,7 +63,7 @@
             <span class="q-ml-md" v-else> MM/DD/YYYY</span>
           </div>
           <div class="row q-mt-sm">
-            <span class="heading-light"> Date of Notified </span>
+            <span class="heading-light col-4"> Date of Notified </span>
             <span class="q-ml-md" v-if="getSelectedClaim.contractInfo">
               {{ getSelectedClaim.contractInfo.date | moment('MM/DD/YYYY') }}
             </span>
@@ -76,26 +85,21 @@
               }}
             </span>
           </div>
-          <div class="row q-mt-sm">
-            <span class="heading-light col-4"> Claim Timeline </span>
-            <span class="q-ml-md"> - </span>
-          </div>
-          <div class="row q-mt-sm">
-            <span class="heading-light col-4"> Loss Description </span>
-            <span class="q-ml-md col" v-if="getSelectedClaim.lossInfo">
-              {{
-                getSelectedClaim.lossInfo.desc
-                  ? getSelectedClaim.lossInfo.desc
-                  : '-'
-              }}
-            </span>
-          </div>
         </div>
       </q-card>
 
       <q-card class="q-ma-md q-pa-md ">
-        <div class="row q-ml-xs ">
-          <span class="text-bold col q-mt-xs"> Claim Deadlines</span>
+        <div class="row q-ml-xs justify-between ">
+          <div class="text-bold  q-mt-xs">Claim Deadlines</div>
+          <div class=" q-mt-xs">
+            <q-icon
+              class="q-ml-xs"
+              name="edit"
+              size="xs"
+              color="primary"
+              @click="claimDeadline = true"
+            />
+          </div>
         </div>
         <div class=" q-ml-xs ">
           <div class="row q-mt-sm">
@@ -173,11 +177,13 @@
             </span>
           </div>
           <div class="row q-mt-sm">
-            <span class="heading-light col-4">
-              Description of Loss
-            </span>
-            <span class="q-ml-md col-4">
-              -
+            <span class="heading-light col-4"> Loss Description </span>
+            <span class="q-ml-md col" v-if="getSelectedClaim.lossInfo">
+              {{
+                getSelectedClaim.lossInfo.desc
+                  ? getSelectedClaim.lossInfo.desc
+                  : '-'
+              }}
             </span>
           </div>
         </div>
@@ -233,6 +239,118 @@
         </div>
       </div>
     </div>
+    <q-dialog
+      v-model="claimDeadline"
+      persistent
+      :maximized="true"
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+      <q-card>
+        <CustomBar
+          @closeDialog="claimDeadline = false"
+          :dialogName="'Edit Claim Deadlines'"
+        />
+        <div class="q-ma-sm mobile-container-page">
+          <q-card class="q-mx-sm">
+            <div class="q-px-md">
+              <q-input
+                class="q-py-sm"
+                label="Trolling Date /Statute Deadline"
+              />
+              <q-input class="q-py-sm" label="Recoverable Depreciation Due" />
+              <q-input class="q-py-sm" label="CRN Deadline" />
+            </div>
+          </q-card>
+        </div>
+        <q-btn
+          label="Save"
+          color="primary"
+          class="button-width-90"
+          @click="onSaveButtonClick()"
+          size="'xl'"
+        />
+      </q-card>
+    </q-dialog>
+    <!-- Claim Summary -->
+    <q-dialog
+      v-model="claimSummary"
+      persistent
+      :maximized="true"
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+      <q-card>
+        <CustomBar
+          @closeDialog="claimSummary = false"
+          :dialogName="'Edit Claim Deadlines'"
+        />
+        <div class="q-ma-sm mobile-container-page">
+          <q-card class="q-mx-sm">
+            <div class="q-px-sm">
+              <q-input
+                class="q-py-sm"
+                v-model="policyInfo.claimNumber"
+                label="Claim Number"
+              />
+              <q-input
+                class="q-py-sm"
+                v-model="policyInfo.claimFee"
+                label="Claim Fee(%)"
+              />
+              <q-input
+                class="q-py-sm"
+                v-model="policyInfo.carrierNotifyDate"
+                label="Date Notified"
+              />
+              <q-input
+                class="q-py-sm"
+                v-model="policyInfo.reasonForClaim"
+                label="Reason For Claim"
+              />
+            </div>
+          </q-card>
+          <q-card class="q-ma-sm">
+            <div class="q-ma-sm">
+              <q-input
+                class="q-py-sm"
+                v-model="policyInfo.sourceOfClaim"
+                label="Source of Claim"
+              />
+              <q-input
+                class="q-py-sm"
+                v-model="policyInfo.contractDetails"
+                label="Contract Date"
+              />
+              <q-input
+                class="q-py-sm"
+                v-model="policyInfo.dateOfFirstContact"
+                label="Date of First Contact"
+              />
+              <q-input
+                class="q-py-sm"
+                v-model="policyInfo.timeOfFirstContact"
+                label="Time of First Contact"
+              />
+            </div>
+          </q-card>
+          <q-card class="q-mx-sm">
+            <div class="q-ma-sm">
+              <q-input class="q-py-sm" label="Days Open" />
+              <q-input class="q-py-sm" label="Since Loss" />
+              <q-input class="q-py-sm" label="Since Notification" />
+            </div>
+          </q-card>
+        </div>
+        <q-btn
+          label="Save"
+          color="primary"
+          class="button-width-90"
+          @click="onSaveButtonClick()"
+          size="'xl'"
+        />
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -246,7 +364,58 @@ export default {
   name: 'Claims',
   components: { CustomBar, ClaimDetail },
   data() {
-    return { rating: 1 };
+    return {
+      rating: 1,
+      claimDeadline: false,
+      claimSummary: false,
+
+      policyInfo: {
+        carrier: {
+          id: '',
+          value: ''
+        },
+        claimFee: '',
+        reasonForClaim: '',
+        sourceOfClaim: '',
+        contractDetails: '',
+        dateOfFirstContact: '',
+        timeOfFirstContact: '',
+        carrierNotifyDate: '',
+        number: '',
+        isClaimFiled: true,
+        isForcedPlaced: true,
+        claimNumber: '',
+        category: {
+          id: '',
+          value: '',
+          machineValue: ''
+        },
+        type: {
+          id: '',
+          value: '',
+          machineValue: ''
+        },
+        effectiveDate: '',
+        expirationDate: '',
+        // limitCoverage: {
+        //   dwelling: 12.3,
+        //   otherStructure: 12.34,
+        //   content: 12.21,
+        //   lossOfUse: 12.1
+        // },
+        // deductibleAmount: 45,
+        // depreciation: 1234.09,
+        // declaration: {
+        //   isDeclared: true,
+        //   fileInfo: {
+        //     id: '243721c8-4f4c-11eb-ae93-0242ac130002',
+        //     value: 'policy_declared.pdf'
+        //   }
+        // },
+        priorPayment: 1234.09,
+        limitReason: 'reason for limits or denial'
+      }
+    };
   },
 
   computed: {
