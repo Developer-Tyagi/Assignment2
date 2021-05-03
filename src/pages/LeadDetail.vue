650ed785b3d86dc7e7b6edca5eb24fd7a7460423
sonali <sonalisaxena9755@gmail.com>
2021-05-03 16:13:39 +0530

    lead detail component deleted because now we are using accordion so no need to make component

--- src/pages/LeadDetail.vue
+++ src/pages/LeadDetail.vue
@@ -2,7 +2,71 @@
   <q-page>
     <div class="mobile-container-page-without-search">
       <div>
-        <LeadDetail />
+        <div class=" q-mx-lg q-mt-md">
+          <div class="q-ml-none text-primary">
+            {{ selectedLead['primaryContact']['fname'] }}
+            {{ selectedLead.primaryContact.lname }}
+          </div>
+
+          <div class="row  q-mt-sm">
+            <span class="heading-light col-3"> Email </span>
+            <span
+              class="q-ml-md col clickLink"
+              @click="onEmailClick(selectedLead.primaryContact.email, $event)"
+            >
+              {{
+                selectedLead.primaryContact.email
+                  ? selectedLead.primaryContact.email
+                  : '-'
+              }}</span
+            >
+          </div>
+          <div class="row  q-mt-sm">
+            <span class="heading-light col-3"> Phone Number </span>
+            <span
+              class="q-ml-md col clickLink"
+              @click="
+                onPhoneNumberClick(
+                  selectedLead.primaryContact.phoneNumber[0].number,
+                  $event
+                )
+              "
+            >
+              {{
+                selectedLead.primaryContact.phoneNumber[0].number
+                  ? selectedLead.primaryContact.phoneNumber[0].number
+                  : '-'
+              }}</span
+            >
+          </div>
+
+          <div class="row  q-mt-sm" v-if="selectedLead.leadSource">
+            <span class="heading-light col-3"> Lead Source </span>
+            <span class="q-ml-md col">
+              {{
+                selectedLead.leadSource.detail
+                  ? selectedLead.leadSource.detail
+                  : '-'
+              }}</span
+            >
+          </div>
+          <div class="row  q-mt-sm" v-if="selectedLead.organizationName">
+            <span class="heading-light col-3"> Organization Name </span>
+            <span class="q-ml-md col">
+              {{ selectedLead.organizationName }}</span
+            >
+          </div>
+          <div class="row  q-mt-sm" v-if="selectedLead.inspectionInfo">
+            <span class="heading-light col-3"> Inspection Type </span>
+            <span class="q-ml-md col">
+              {{
+                selectedLead.inspectionInfo.value
+                  ? selectedLead.inspectionInfo.value
+                  : '-'
+              }}</span
+            >
+          </div>
+        </div>
       </div>
       <q-list bordered class="q-mt-xs">
         <q-expansion-item
@@ -151,14 +215,14 @@
 <script>
 import { mapActions, mapGetters, mapMutations } from 'vuex';
 import CustomBar from 'components/CustomBar';
-import LeadDetail from 'components/LeadDetail';
+
 import { onEmailClick, onPhoneNumberClick, sendMap } from '@utils/clickable';
 export default {
   data() {
     return {};
   },
 
-  components: { CustomBar, LeadDetail },
+  components: { CustomBar },
   created() {
     this.getLeadDetails(this.$route.params.id);
   },
