65d8fa664136668c692c99e33f255e0df256192f
himanshu.ranjan1 <himanshu.ranjan@cilalabs.com>
2021-05-03 15:37:20 +0530

    added cancel button

--- src/pages/ClaimNotes.vue
+++ src/pages/ClaimNotes.vue
@@ -76,6 +76,13 @@
         <q-card-actions align="right">
           <q-btn
             flat
+            label="Cancel"
+            color="primary"
+            v-close-popup
+            @click="alert = false"
+          ></q-btn>
+          <q-btn
+            flat
             label="Delete"
             color="primary"
             v-close-popup
