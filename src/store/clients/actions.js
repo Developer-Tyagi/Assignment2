3c096612136fa38b075c34475436001b96c54cff
sonali <sonalisaxena9755@gmail.com>
2021-05-17 21:06:57 +0530

    temp checkout

--- src/store/clients/actions.js
+++ src/store/clients/actions.js
@@ -362,3 +362,26 @@ export async function editClientNotes({ dispatch, state }, payload) {
     });
   }
 }
+
+export async function updateClaimTimeline({ dispatch, state }, payload) {
+  dispatch('setLoading', true);
+  try {
+    const { data } = await request.patch(
+      `/claims/${payload.id}/phases/${payload.phase}`,
+      buildApiData('clients', payload.data)
+    );
+    dispatch('setLoading', false);
+    dispatch('setNotification', {
+      type: 'positive',
+      message: 'Phase updated successfully !'
+    });
+    return data;
+  } catch (e) {
+    console.log(e);
+    dispatch('setLoading', false);
+    dispatch('setNotification', {
+      type: 'negative',
+      message: 'failed to update phase'
+    });
+  }
+}
