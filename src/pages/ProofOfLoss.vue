a08afb8575c7a29f3ab73764a7db747a90102523
sonali <sonalisaxena9755@gmail.com>
2021-05-22 11:52:21 +0530

    temp checkout

---
+++ src/pages/ProofOfLoss.vue
@@ -0,0 +1,136 @@
+<template>
+  <q-page>
+    <q-icon
+      @click="onClickTopMenu"
+      name="more_vert"
+      size="sm"
+      class="icon-top"
+    />
+    <!-- {{ proofOfLoss }} -->
+
+    <div class="vertical-center q-px-md q-py-sm ">
+      <div class="row">
+        <q-icon name="picture_as_pdf" size="sm" color="primary" />
+        <span class="q-pl-md"> {{ proofOfLoss.documents.name }}</span>
+        <!-- {{ proofOfLoss.documents.webViewLink }} -->
+        <iframe
+          src="https://drive.google.com/file/d/1afMjjtRBxAGbQr79YUM4NA2JvyxRaEcq/view?usp=drivesdk"
+          width="100%"
+          height="500px"
+        >
+        </iframe>
+
+        <q-icon
+          @click="onClickSideMenu"
+          name="more_vert"
+          size="sm"
+          class="q-ml-auto"
+        />
+      </div>
+    </div>
+
+    <!-- Top Menu Items Dialog -->
+    <q-dialog
+      v-model="menuItemsOptions"
+      :maximized="true"
+      transition-show="slide-up"
+      transition-hide="slide-down"
+      :position="'bottom'"
+    >
+      <q-card style="width: 350px">
+        <q-card-section class="items-center">
+          <div
+            class="q-pa-md heading-light"
+            @click="editClaimNumberDialogOnClick('Claim Number')"
+          >
+            Approve
+          </div>
+          <div
+            class="q-pa-md heading-light"
+            @click="$router.push('/claim-details')"
+          >
+            Edit Claim Details
+          </div>
+          <div
+            class="q-pa-md heading-light"
+            @click="editClaimNumberDialogOnClick('Claim Phase')"
+          >
+            Regenerate
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+    <!-- Side Menu Items Dialog -->
+    <q-dialog
+      v-model="sideMenuOptionsDialog"
+      :maximized="true"
+      transition-show="slide-up"
+      transition-hide="slide-down"
+      :position="'bottom'"
+    >
+      <q-card style="width: 550px; height:200px">
+        <q-card-section class="items-center form-heading q-ml-md">
+          <div class="row q-ml-xl ">
+            <div class="column q-ml-xl">
+              <q-btn
+                name="upload"
+                icon="share"
+                @click="(shareDialog = true), (sideMenuOptionsDialog = false)"
+                text-color="primary"
+                class="q-ml-sm"
+              />
+              <div class="form-heading q-ml-md">Share</div>
+            </div>
+            <div class="column">
+              <q-btn
+                class="q-ml-md"
+                icon="delete"
+                text-color="primary"
+                style="width: 50px"
+                @click="(alert = true), (sideMenuOptionsDialog = false)"
+              />
+              <div class="form-heading q-ml-md">
+                Remove
+              </div>
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </q-page>
+</template>
+
+<script>
+import { mapActions, mapMutations, mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      menuItemsDialog: false,
+      menuItemsOptions: false,
+      sideMenuOptionsDialog: false,
+      link: ''
+    };
+  },
+  computed: {
+    ...mapGetters(['proofOfLoss', 'selectedClaimId'])
+  },
+
+  created() {
+    // this.link = this.proofOfLoss.documents.webViewLink;
+    // console.log(this.link);
+    this.getProofOfLossDoc(this.selectedClaimId);
+  },
+  methods: {
+    ...mapActions(['getProofOfLossDoc']),
+    ...mapMutations(['setLoading']),
+    onClickTopMenu() {
+      this.menuItemsOptions = true;
+      this.allFolder = true;
+    },
+    onClickSideMenu() {
+      this.sideMenuOptionsDialog = true;
+    }
+  }
+};
+</script>
