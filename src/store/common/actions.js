d8f17f3683e90f0c94c3fe8dc66ae1ac068711dc
himanshu.ranjan1 <himanshu.ranjan@cilalabs.com>
2021-06-03 02:37:48 +0530

    report page , added photoId mail in signup page and admin user edit page

--- src/store/common/actions.js
+++ src/store/common/actions.js
@@ -263,25 +263,6 @@ export async function getReportDownload({ commit, dispatch }, payload) {
     });
   }
 }
-// export async function getReportDownload({ dispatch, state }, payload) {
-//   dispatch('setLoading', true);
-//   try {
-//     const { data } = await request.patch(
-//       `/users/${payload.id}/roles`,
-//       buildApiData('users', payload.data)
-//     );
-
-//     dispatch('setLoading', false);
-//   } catch (e) {
-//     console.log(e);
-//     dispatch('setLoading', false);
-//     dispatch('setNotification', {
-//       type: 'negative',
-//       message: e.response[0].title
-//     });
-//     return false;
-//   }
-// }
 
 export async function createDirectories({ dispatch, state }, payload) {
   dispatch('setLoading', true);
