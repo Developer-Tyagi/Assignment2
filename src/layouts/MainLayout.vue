76eae3f9ee43a02276b3c8e7fd1b4c25fb5d21f9
himanshu.ranjan1 <himanshu.ranjan@cilalabs.com>
2021-06-08 09:59:47 +0530

    floating add icon

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,6 +3,75 @@
     <CustomHeader @backButton="onBackButtonClick" />
     <q-page-container>
       <router-view />
+      <q-page-sticky
+        position="bottom-right"
+        :offset="[30, 18]"
+        v-if="
+          $route.name != 'add new leads' &&
+            $route.name != 'Add Claim' &&
+            $route.name != 'create client' &&
+            $route.name != 'edit  lead' &&
+            $route.name != 'vendors'
+        "
+      >
+        <div class="dot ">
+          <q-icon
+            class="q-pa-md"
+            name="add"
+            size="sm"
+            color="white"
+            @click="onClickAddUpIcon"
+          />
+        </div>
+      </q-page-sticky>
+      <q-dialog
+        v-model="openDialog"
+        :maximized="true"
+        transition-show="slide-up"
+        transition-hide="slide-down"
+        :position="'bottom'"
+      >
+        <q-card style="width: 550px; height: 200px">
+          <q-card-section class=" text-center  ">
+            <div class="text-bold">
+              <!-- Add Directly -->
+
+              <q-icon
+                name="expand_more"
+                size="lg"
+                @click="openDialog = false"
+              />
+            </div>
+            <div class="row justify-between q-mt-md">
+              <q-card
+                class="bg-grey-3 q-pa-sm"
+                @click="$router.push('/add-client')"
+                >Add Client</q-card
+              >
+              <q-card
+                class="bg-grey-3 q-pa-sm"
+                @click="$router.push('/add-client')"
+                >Add Claim</q-card
+              >
+              <q-card class="bg-grey-3 q-pa-sm" @click="$router.push('/leads')"
+                >Add Lead</q-card
+              >
+            </div>
+            <div class="row justify-between q-mt-lg">
+              <q-card
+                class="bg-grey-3 q-pa-sm"
+                @click="$router.push('/mortgages')"
+                >Add mortgage</q-card
+              >
+              <q-card
+                class="bg-grey-3 q-pa-sm"
+                @click="$router.push('/carriers')"
+                >Add Carrier</q-card
+              >
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-dialog>
     </q-page-container>
   </q-layout>
 </template>
@@ -12,6 +81,9 @@ import { getCurrentUser } from '@utils/auth';
 export default {
   name: 'MainLayout',
   components: { CustomHeader },
+  data() {
+    return { openDialog: false };
+  },
   methods: {
     onBackButtonClick() {
       this.userRole = getCurrentUser().attributes.roles[0].machineValue;
@@ -33,7 +105,22 @@ export default {
       } else {
         this.$router.go(-1);
       }
+    },
+    onClickAddUpIcon() {
+      this.openDialog = true;
     }
   }
 };
 </script>
+<style lang="scss">
+.icon-width {
+  width: 100px;
+}
+.dot {
+  height: 55px;
+  width: 55px;
+  background-color: $primary;
+  border-radius: 50%;
+  display: inline-block;
+}
+</style>
