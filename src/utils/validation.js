a5d5e0c5edb2ac98f0bc29ae5f6a5d9410af37da
Animesh <animesh@cilalabs.com>
2021-06-11 13:20:20 +0530

    changes

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,5 +1,5 @@
 export function validateEmail(mail) {
-  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (regex.test(mail)) {
     return true;
   }
@@ -8,6 +8,7 @@ export function validateEmail(mail) {
 
 export function validateUrl(url) {
   const regex = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/;
+
   if (url) {
     if (regex.test(url)) {
       return true;
@@ -20,6 +21,7 @@ export function validateUrl(url) {
 
 export function validateDate(val) {
   const regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
+
   if (val) {
     if (regex.test(val)) {
       return true;
