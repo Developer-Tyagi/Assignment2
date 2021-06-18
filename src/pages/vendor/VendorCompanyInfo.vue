7d393ea9826310e7b6a9671bcbe39199cd3a4e80
sonali <sonalisaxena9755@gmail.com>
2021-06-18 14:17:59 +0530

    accordion added in vendor info page

--- src/pages/vendor/VendorCompanyInfo.vue
+++ src/pages/vendor/VendorCompanyInfo.vue
@@ -1,115 +1,124 @@
 <template>
-  <q-page>
-    <div class="listing-height q-pa-md mobile-container-page">
-      <span class="text-bold text-h6 q-ml-md">
-        {{ selectedVendor.name ? selectedVendor.name : '-' }}
-      </span>
-      <q-card class="q-ma-sm q-pa-sm">
-        <div class="q-my-auto row q-mt-xs">
-          <q-icon
-            size="xs"
-            name="create "
-            color="primary"
-            class="q-ml-auto"
-            @click="onEdit"
-          ></q-icon>
+  <div>
+    <span class="text-bold text-h6 ">
+      {{ selectedVendor.name ? selectedVendor.name : '-' }}
+    </span>
 
-          <q-icon
-            class="q-my-auto"
-            name="delete"
-            size="xs"
-            color="primary"
-            @click="onDelete"
-          />
-        </div>
-        <div class="row q-mt-sm">
-          <span class="heading-light col-3">Address Details</span>
-          <span class="col-7 q-ml-md" v-if="selectedVendor.mailingAddress">
-            <div class="row">
-              {{
-                selectedVendor.mailingAddress.houseNumber
-                  ? selectedVendor.mailingAddress.houseNumber
-                  : '-'
-              }}
-              ,
-              {{
-                selectedVendor.mailingAddress.streetAddress
-                  ? selectedVendor.mailingAddress.streetAddress
-                  : '-'
-              }}
-            </div>
-            <div>
-              {{
-                selectedVendor.mailingAddress.addressLocality
-                  ? selectedVendor.mailingAddress.addressLocality
-                  : '-'
-              }}
-              ,
-              {{
-                selectedVendor.mailingAddress.addressRegion
-                  ? selectedVendor.mailingAddress.addressRegion
-                  : '-'
-              }}
-            </div>
-            <div class="row">
-              {{
-                selectedVendor.mailingAddress.addressCountry
-                  ? selectedVendor.mailingAddress.addressCountry
-                  : '-'
-              }},
-              {{
-                selectedVendor.mailingAddress.postalCode
-                  ? selectedVendor.mailingAddress.postalCode
-                  : '-'
-              }}
-              <q-icon
-                name="place"
-                color="primary"
-                @click="sendMap(selectedVendor.mailingAddress)"
-                class="edit-icon"
-              ></q-icon></div
-          ></span>
-        </div>
-        <div class="row q-mt-sm" v-if="selectedVendor.email">
-          <span class="heading-light col-3"> Email </span>
-          <span
-            class="q-ml-md col clickLink"
-            @click="onEmailClick(selectedVendor.email, $event)"
-          >
-            {{ selectedVendor.email ? selectedVendor.email : '-' }}</span
-          >
-        </div>
-        <div class="row q-mt-sm" v-if="selectedVendor.phoneNumber">
-          <span class="heading-light col-3"> Phone Number </span>
-          <span class="col q-ml-md" v-if="selectedVendor.phoneNumber.type">
-            {{ selectedVendor.phoneNumber.type }} :
-            <span
-              class="clickLink"
-              @click="
-                onPhoneNumberClick(selectedVendor.phoneNumber.number, $event)
-              "
-              >{{ selectedVendor.phoneNumber.number }}</span
-            ></span
-          >
-        </div>
-        <div class="row q-mt-sm" v-if="selectedVendor.info">
-          <span class="heading-light col-3"> Website: </span>
-          <span class="q-ml-md col">
-            {{
-              selectedVendor.info.website ? selectedVendor.info.website : '-'
-            }}</span
-          >
-        </div>
-        <div class="row q-mt-sm" v-if="selectedVendor.info">
-          <span class="heading-light col-3"> Notes: </span>
-          <span class="q-ml-md col">
-            {{
-              selectedVendor.info.notes ? selectedVendor.info.notes : '-'
-            }}</span
-          >
-        </div>
-      </q-card>
+    <div class="q-my-auto row q-mt-xs">
+      <q-icon
+        size="xs"
+        name="create "
+        color="primary"
+        class="q-ml-auto"
+        @click="onEdit"
+      ></q-icon>
+
+      <q-icon
+        class="q-my-auto"
+        name="delete"
+        size="xs"
+        color="primary"
+        @click="onDelete"
+      />
     </div>
+    <div class="row">
+      <span class="heading-light col-4">Address Details</span>
+      <span class="col-7 q-ml-md" v-if="selectedVendor.mailingAddress">
+        <div class="row">
+          {{
+            selectedVendor.mailingAddress.houseNumber
+              ? selectedVendor.mailingAddress.houseNumber
+              : '-'
+          }}
+          ,
+          {{
+            selectedVendor.mailingAddress.streetAddress
+              ? selectedVendor.mailingAddress.streetAddress
+              : '-'
+          }}
+        </div>
+        <div>
+          {{
+            selectedVendor.mailingAddress.addressLocality
+              ? selectedVendor.mailingAddress.addressLocality
+              : '-'
+          }}
+          ,
+          {{
+            selectedVendor.mailingAddress.addressRegion
+              ? selectedVendor.mailingAddress.addressRegion
+              : '-'
+          }}
+        </div>
+        <div class="row">
+          {{
+            selectedVendor.mailingAddress.addressCountry
+              ? selectedVendor.mailingAddress.addressCountry
+              : '-'
+          }},
+          {{
+            selectedVendor.mailingAddress.postalCode
+              ? selectedVendor.mailingAddress.postalCode
+              : '-'
+          }}
+          <q-icon
+            name="place"
+            color="primary"
+            @click="sendMap(selectedVendor.mailingAddress)"
+            class="edit-icon"
+          ></q-icon></div
+      ></span>
+      <span class="col-7 q-ml-md" v-else>-</span>
+    </div>
+
+  
+    <div class="q-mt-sm row" v-if="selectedVendor.email">
+      <div class="heading-light q-mt-none col-xs-4 lightHeading">
+        Email
+      </div>
+      <div
+        class="column q-ml-md clickLink"
+        @click="onEmailClick(selectedVendor.email, $event)"
+      >
+        {{ selectedVendor.email ? selectedVendor.email : '-' }}
+      </div>
+    </div>
+     <div class="q-mt-sm row" v-if="selectedVendor.phoneNumber">
+      <div class="heading-light q-mt-none col-xs-4 lightHeading">
+        Phone Number
+      </div>
+      <div
+      v-if="selectedVendor.phoneNumber.type"
+        class="column q-ml-md " 
+        @click="onEmailClick(selectedVendor.email, $event)"
+      >
+       {{ selectedVendor.phoneNumber.type }} :
+      </div>
+        <span
+          class="clickLink"
+          @click="onPhoneNumberClick(selectedVendor.phoneNumber.number, $event)"
+          >{{ selectedVendor.phoneNumber.number }}</span
+        ></span
+      >
+    </div>
+  
+
+   
+    <div class="row q-mt-sm" v-if="selectedVendor.info">
+      <span class="heading-light col-3"> Website: </span>
+      <span class="q-ml-md col">
+        {{
+          selectedVendor.info.website ? selectedVendor.info.website : '-'
+        }}</span
+      >
+    </div>
+    <div class="row q-mt-sm" v-if="selectedVendor.info">
+      <span class="heading-light col-3"> Notes: </span>
+      <span class="q-ml-md col">
+        {{ selectedVendor.info.notes ? selectedVendor.info.notes : '-' }}</span
+      >
+    </div>
+
     <q-dialog
       v-model="addMortgageDialog"
       :maximized="true"
@@ -124,7 +133,7 @@
         />
       </q-card>
     </q-dialog>
-  </q-page>
+  </div>
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex';
@@ -132,6 +141,7 @@ import { onEmailClick, onPhoneNumberClick, sendMap } from '@utils/clickable';
 import AddVendor from 'components/AddVendor';
 import { constants } from '@utils/constant';
 export default {
+  name: 'VendorCompanyInfo',
   components: { AddVendor },
   data() {
     return { addMortgageDialog: false, constants: constants };
