844702fcc0d628fb4e8ad9ca505de30f2ee4a96e
himanshu.ranjan1 <himanshu.ranjan@cilalabs.com>
2021-07-08 01:59:15 +0530

    report page Bug

--- src/pages/Reports.vue
+++ src/pages/Reports.vue
@@ -49,7 +49,6 @@ export default {
     return {
       Value: 'pdf',
       TypeOfClient: '',
-      // options: ['claim', 'client']
       options: [
         { value: 'Claims', machineValue: 'claims' },
         { value: 'Clients', machineValue: 'clients' }
