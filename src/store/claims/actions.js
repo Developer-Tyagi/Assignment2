c5dd5f5db8e8cb3134afafc6edb085e51c2db87f
himanshu.ranjan1 <himanshu.ranjan@cilalabs.com>
2021-07-30 09:58:50 +0530

    changes

--- src/store/claims/actions.js
+++ src/store/claims/actions.js
@@ -1533,3 +1533,26 @@ export async function taskComplete({ dispatch, state }, payload) {
     return false;
   }
 }
+
+export async function taskUncomplete({ dispatch, state }, payload) {
+  dispatch('setLoading', true);
+  try {
+    const { data } = await request.post(
+      `/claims/${payload.claimID}/tasks/${payload.taskId}/uncomplete
+`
+    );
+    dispatch('setLoading', false);
+    dispatch('setNotification', {
+      type: 'positive',
+      message: 'Task Uncompleted successfully!'
+    });
+  } catch (e) {
+    console.log(e);
+    dispatch('setLoading', false);
+    dispatch('setNotification', {
+      type: 'negative',
+      message: e.response[0].title
+    });
+    return false;
+  }
+}
