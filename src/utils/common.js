8b849cece1d758f8ab6cceb195f57ce1685ed5dc
ritesh_kumar_pandey <ritesh.pandey@cilalabs.com>
2021-08-27 12:59:28 +0530

    update in address display format

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,7 @@
+import AddressService from '@utils/country';
+const addressService = new AddressService();
+var states = addressService.getStatesShortName('United States');
+
 export function getBase64(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -5,4 +9,9 @@ export function getBase64(file) {
     reader.onload = () => resolve(reader.result);
     reader.onerror = error => reject(error);
   });
+}
+export function toGetStateShortName(state) {
+  for (let i = 0; i < states.length; i++) {
+    if (state == states[i].name) return states[i].shortCode;
+  }
 }
--- src/utils/country.js
+++ src/utils/country.js
@@ -1,12 +1,18 @@
-import countryRegionData from "country-region-data";
+import countryRegionData from 'country-region-data';
 
 export default class AddressService {
+  getCountries() {
+    return countryRegionData.map(item => item.countryName);
+  }
 
-    getCountries() {
-        return countryRegionData.map((item) => item.countryName)
-    }
-
-    getStates(country) {
-        return countryRegionData.filter((item) => item.countryName === country)[0]["regions"].map((item) => item.name);
-    }
+  getStates(country) {
+    return countryRegionData
+      .filter(item => item.countryName === country)[0]
+      ['regions'].map(item => item.name);
+  }
+  getStatesShortName(country) {
+    return countryRegionData
+      .filter(item => item.countryName === country)[0]
+      ['regions'].map(val => ({ ...val }));
+  }
 }
