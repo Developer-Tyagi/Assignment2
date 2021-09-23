1aacfcf6b86b684bd6906a2e9a8d38ce08c1efe5
ritesh_kumar_pandey <ritesh.pandey@cilalabs.com>
2021-09-23 18:34:14 +0530

    merge conflict resolve

--- src/components/CustomHeader.vue
+++ src/components/CustomHeader.vue
@@ -428,7 +428,7 @@ export default {
       await this.getAccess();
       this.createSidebarMenuItems();
     }
-    if (this.$q.screen.width > 2731) {
+    if (this.$q.screen.width > 1365) {
       this.isLeftSidePanelOpen = true;
     }
   }
