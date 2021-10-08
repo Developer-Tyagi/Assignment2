bab5025bf688767ab9631aad4e0c4661af4a44be
ritesh_kumar_pandey <ritesh.pandey@cilalabs.com>
2021-10-08 08:47:14 +0530

    mainlayout fix

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,161 @@
+<template>
+  <q-layout view="lhr lpR lfr">
+    <CustomHeader @backButton="onBackButtonClick" />
+    <div
+      class="text-black
+    "
+    ></div>
+
+    <q-page-container>
+      <router-view />
+      <div id="navbar">
+        <q-page-sticky
+          position="bottom-right"
+          :offset="[30, 18]"
+          v-if="
+            $route.name != 'add new leads' &&
+              $route.name != 'create client' &&
+              $route.name != 'edit  lead' &&
+              $route.name != 'vendors' &&
+              $route.name != 'configuration  ' &&
+              $route.name != 'manage users' &&
+              $route.name != 'admin' &&
+              $route.name != 'reports' &&
+              $route.name != 'signup' &&
+              $route.name != 'set-password' &&
+              $route.name != 'payment' &&
+              $route.name != 'loss info' &&
+              $route.name != 'Documents' &&
+              $route.name != 'properties and claims ' &&
+              $route.name != 'notes' &&
+              $route.name != 'Company Personnel' &&
+              $route.name != 'add new lead' &&
+              $route.name != 'lead details' &&
+              $route.name != 'Leads' &&
+              $route.name != 'claim details' &&
+              $route.name != 'dashboard' &&
+              $route.name != 'Add Claim'
+          "
+        >
+          <div class="dot" v-if="checkUserRoleType() == false">
+            <q-icon
+              class="q-pa-md"
+              name="add"
+              size="sm"
+              color="white"
+              @click="onClickAddUpIcon"
+            />
+          </div>
+        </q-page-sticky>
+      </div>
+      <q-dialog
+        v-model="openDialog"
+        :maximized="true"
+        transition-show="slide-up"
+        transition-hide="slide-down"
+        :position="'bottom'"
+      >
+        <AddOptions />
+      </q-dialog>
+    </q-page-container>
+  </q-layout>
+</template>
+<script>
+import CustomHeader from 'components/CustomHeader';
+import { getCurrentUser } from '@utils/auth';
+import { mapGetters, mapMutations } from 'vuex';
+import AddOptions from 'components/AddOptions';
+export default {
+  name: 'MainLayout',
+  components: { CustomHeader, AddOptions },
+  data() {
+    return { openDialog: false };
+  },
+  computed: {
+    ...mapGetters(['isEdit'])
+  },
+  methods: {
+    ...mapMutations(['isLastRouteEdit', 'setCameraIcon']), //'setCameraIcon' is a function which check weather we need to show the camera Icon on Vendor Document Page  or Not , It return True when we redirect from the
+    //the camera option  in dashboard page and it is false when we redirect from scan and upload option
+
+    checkUserRoleType() {
+      if (
+        getCurrentUser().attributes.roles[0].machineValue == 'estimator' ||
+        getCurrentUser().attributes.roles[0].machineValue == 'vendor'
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    onBackButtonClick() {
+      this.userRole = getCurrentUser().attributes.roles[0].machineValue;
+      const route = this.$router.currentRoute.fullPath.split('/')[1];
+      switch (route) {
+        case route == 'clients' ||
+          route == 'vendors' ||
+          route == 'admin' ||
+          route == 'settings' ||
+          route == 'claims' ||
+          route == 'mortgages' ||
+          route == 'add-client' ||
+          route == 'carriers':
+          this.$router.push('/dashboard');
+          break;
+        case route == 'leads' || route == 'add-lead':
+          this.$router.go(-1);
+          break;
+        case route == 'mortgage-details':
+          this.$router.push('/mortgages');
+          break;
+        case route == 'carrier-details':
+          this.$router.push('/carriers');
+          break;
+        case route == 'view-client':
+          this.$router.push('/clients');
+          break;
+        case route == 'claim-details':
+          this.$router.push('/claims');
+          break;
+        case (route == 'claim-summary' && this.userRole == 'estimator') ||
+          this.userRole == 'vendor':
+          this.$router.push('/claims');
+          break;
+        case route == 'claims':
+          this.$store.commit('setClaims');
+          break;
+        default:
+          if (this.isEdit) {
+            this.isLastRouteEdit(false);
+            this.$router.go(-3);
+          } else if (route == 'vendor-document') {
+            this.setCameraIcon('false'); // the camera icon become Disappeared when we press back button
+            this.$router.push('/dashboard');
+          } else {
+            this.$router.go(-1);
+          }
+      }
+    },
+    onClickAddUpIcon() {
+      this.openDialog = true;
+    }
+  }
+};
+</script>
+<style lang="scss">
+.icon-width {
+  width: 100px;
+}
+.dot {
+  height: 55px;
+  width: 55px;
+  background-color: $primary;
+  border-radius: 50%;
+  display: inline-block;
+}
+.card {
+  max-width: 100%;
+  height: 40px;
+  border-radius: 5px;
+}
+</style>
