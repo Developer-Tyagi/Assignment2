d6a3cae72f71f4c2ebb36b9ef60ba8cd6fa4e793
Rudra <ramrudra.p@gmail.com>
2021-11-15 15:54:55 +0530

    IOS UI fixes

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -12,7 +12,7 @@
         cardHeight
         q-pb-lg
       "
-      style="border-radius: 0 0 40px 40px"
+      style="border-radius: 0 0 40px 40px; margin-top: -25px;"
     >
       <div class="row col-4 justify-around">
         <div
@@ -142,15 +142,23 @@
       </div>
     </div>
     <!-- footer -->
-    <div class="row justify-center footer-shadow"></div>
+    <!-- <div class="row justify-center footer-shadow"></div> -->
+    <div class="text-center">
+      <img
+        src="~assets/header_shadow.png"
+        class="relative-position"
+        style="width: 90%; height: 50px; margin-top: -22px"
+      />
+    </div>
     <div
       class="row justify-between q-px-md q-pt-xs"
-      style="background-color: #8f8f8f"
+      style="background-color: #8f8f8f; margin-top: -30px;"
     >
       <div class="col-3 column">
         <img
           class="col"
           src="~assets/Profile.svg"
+          height=" 50px"
           @click="$router.push('/edit-profile')"
         />
         <div class="col text-center text-white text-caption">PROFILE</div>
