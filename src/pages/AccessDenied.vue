d76ddb2cbdd06eddfc12a407b48e2cc70f94ab67
umang.gupta <umang.gupta@cilalabs.com>
2022-02-08 20:47:17 +0530

    added logout fixes

--- src/pages/AccessDenied.vue
+++ src/pages/AccessDenied.vue
@@ -18,19 +18,53 @@
       <q-btn style="width: 50%; margin: auto" @click="goToDashboard"
         >Go to Dashboard</q-btn
       >
+      <span class="text-center text-subtitle2 q-my-lg">OR</span>
+      <q-btn
+        color="primary"
+        class=""
+        style="width: 50%; margin: auto"
+        @click="logout()"
+        >Logout</q-btn
+      >
     </div>
   </q-page>
 </template>
 <script>
 import { isMobile } from 'src/utils/common';
+import {
+  removeToken,
+  removeCurrentUser,
+  removeFCMToken,
+  getFCMToken
+} from '../utils/auth';
+import { removeFirebaseToken } from '@utils/firebase';
+import { mapActions } from 'vuex';
 export default {
   methods: {
+    ...mapActions(['deletePushNotificationToken']),
+
     goToDashboard() {
       if (isMobile()) {
         this.$router.push('/dashboard');
       } else {
         this.$router.push('/admin');
       }
+    },
+    removeToken,
+    removeCurrentUser,
+    removeFCMToken,
+    getFCMToken,
+    removeFirebaseToken,
+    async logout() {
+      if (this.getFCMToken()) {
+        await this.deletePushNotificationToken(this.getFCMToken());
+        await this.removeFCMToken();
+      }
+      await this.removeFirebaseToken();
+
+      this.removeToken();
+      this.removeCurrentUser();
+      this.$router.push('/login');
     }
   }
 };
