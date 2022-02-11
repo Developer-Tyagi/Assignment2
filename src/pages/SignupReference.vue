76cc5a95c0eccc3c4c6bc4acd8c182eb81d375c5
umang@cilalabs <umang.gupta@cilalabs.com>
2022-02-11 17:12:45 +0530

    onboard workflow updates

--- src/pages/SignupReference.vue
+++ src/pages/SignupReference.vue
@@ -9,21 +9,18 @@
           <div class="col-3 q-pa-md">
             <q-img
               class="web-menu-claim-guru-logo"
-              :src="getImage('claimguru_new_logo.png')"
+              :src="getImage('Logo.svg')"
             />
           </div>
         </div>
-        <div class="column justify-center text-h4 text-weight-bolder">
-          <div style="text-align: center">
-            The First and Only Catastrophe -Proof
+        <div class="column justify-center text-h5 text-weight-bolder">
+          <div class="q-px-xl q-mx-xl" style="text-align: center ">
+            The First and Only Catastrophe -Proof Claim management System for
+            Public Adjusters
           </div>
-          <div style="text-align: center">
-            Claim management System for Public
-          </div>
-          <div style="text-align: center">Adjusters</div>
         </div>
-        <div class="row q-ml-xl">
-          <div class="text-h6 text-weight-bolder">
+        <div class="row q-ml-xl q-mt-xl">
+          <div class="text-h5 text-weight-bolder">
             <span
               v-if="
                 isPackageSelected.id1 === '' && isPackageSelected.id2 === ''
@@ -38,13 +35,13 @@
             >
           </div>
         </div>
-        <div class="row wrap justify-around items-start content-start q-mt-md">
-          <q-card
-            class="my-card col-md-4 col-sm-12 col-xs-12"
-            @click="onPackageSelection('Firm')"
-            :class="{ 'card-highlighter': isPackageSelected.id1 }"
-          >
-            <q-card-section>
+        <div class="row justify-between q-mt-md q-px-xl q-mx-xl">
+          <div class="col-5">
+            <q-card
+              class="my-card col-md-4 col-sm-12 col-xs-12"
+              @click="onPackageSelection('Firm')"
+              :class="{ 'card-highlighter': isPackageSelected.id1 }"
+            >
               <div
                 class="fit row wrap justify-around items-start content-start"
               >
@@ -52,15 +49,14 @@
                   round
                   dense
                   flat
-                  color="orange"
+                  color="primary"
                   icon="business"
                   size="22px"
                 />
               </div>
-            </q-card-section>
-            <q-card-section>
+
               <div
-                class="text-h5 text-weight-bolder"
+                class="text-h6 text-weight-bolder"
                 style="text-align: center"
               >
                 Firm Package
@@ -71,45 +67,39 @@
               >
                 $200/month
               </div>
-            </q-card-section>
-            <q-card-section>
               <div
-                class="text-subtitle1 text-weight-bolder"
+                class="text-subtitle1 text-weight-bolder q-mb-sm"
                 style="text-align: center; color: #ef5926"
               >
                 Start your 30 day free trial
               </div>
-            </q-card-section>
-            <q-card-section>
+
               <div
-                class="text-subtitle1 col-md-4 col-xs-12 col-sm-12"
+                class=" row text-subtitle1 q-pl-xl col-md-4 col-xs-12 col-sm-12"
                 style="text-align: justify"
                 v-for="firmPackage in firmPackages"
                 :key="firmPackage"
               >
-                <q-btn round dense flat color="orange" icon="check_circle" />
+                <q-btn round dense flat color="primary" icon="check_circle" />
                 {{ firmPackage }}
               </div>
-            </q-card-section>
-            <q-card-section>
-              <div class="q-ml-lg text-blue-grey text-subtitle1">
+
+              <div class="q-ml-lg q-mt-sm text-blue-grey text-subtitle1">
                 Additional licenses at $50/month
               </div>
-            </q-card-section>
-            <q-card-section>
+
               <div class="q-ml-lg q-mb-lg text-blue-grey text-caption">
                 * Vendor roles are limited
               </div>
-            </q-card-section>
-          </q-card>
-
-          <q-card
-            id="IndividualPackage"
-            class="my-card col-md-4 col-sm-12 col-xs-12"
-            @click="onPackageSelection('Individual')"
-            :class="{ 'card-highlighter': isPackageSelected.id2 }"
-          >
-            <q-card-section>
+            </q-card>
+          </div>
+          <div class="col-5">
+            <q-card
+              id="IndividualPackage"
+              class="my-card col-md-4 col-sm-12 col-xs-12"
+              @click="onPackageSelection('Individual')"
+              :class="{ 'card-highlighter': isPackageSelected.id2 }"
+            >
               <div
                 class="fit row wrap justify-around items-start content-start"
               >
@@ -118,14 +108,13 @@
                   dense
                   flat
                   icon="portrait"
-                  color="orange"
+                  color="primary"
                   size="22px"
                 />
               </div>
-            </q-card-section>
-            <q-card-section>
+
               <div
-                class="text-h5 text-weight-bolder"
+                class="text-h6 text-weight-bolder"
                 style="text-align: center"
               >
                 Individual Package
@@ -136,30 +125,30 @@
               >
                 $99/month
               </div>
-            </q-card-section>
-            <q-card-section>
+
               <div
                 class="text-subtitle1 text-weight-bold"
                 style="text-align: center; color: #ef5926"
               >
                 Start your 30 day free trial
               </div>
-            </q-card-section>
-            <q-card-section style="min-height: 270px">
-              <div
-                class="text-subtitle1 col-md-4 col-xs-12 col-sm-12"
-                v-for="individualPackage in individualPackages"
-                :key="individualPackage"
-              >
-                <q-btn round dense flat color="orange" icon="check_circle" />
-                {{ individualPackage }}
-              </div>
-            </q-card-section>
 
-            <q-card-section></q-card-section>
-          </q-card>
+              <q-card-section style="min-height: 180px">
+                <div
+                  class="q-ml-lg text-subtitle1 col-md-4 col-xs-12 col-sm-12"
+                  v-for="individualPackage in individualPackages"
+                  :key="individualPackage"
+                >
+                  <q-btn round dense flat color="primary" icon="check_circle" />
+                  {{ individualPackage }}
+                </div>
+              </q-card-section>
+
+              <q-card-section></q-card-section>
+            </q-card>
+          </div>
         </div>
-        <div class="row q-pt-md">
+        <div class="row q-pt-md q-mt-md q-px-xl q-mx-xl full-width">
           <div class="col-1"></div>
           <div class="col-10">
             <q-card style="border-radius: 20px" class="row">
@@ -168,7 +157,7 @@
                   round
                   dense
                   flat
-                  color="orange"
+                  color="primary"
                   icon="business"
                   size="22px"
                 />
@@ -180,7 +169,7 @@
                 <a class="text-deep-orange" href="">Contact us</a>
               </q-card-section>
               <q-card-section>
-                <q-btn round dense flat color="orange" icon="check_circle" />
+                <q-btn round dense flat color="primary" icon="check_circle" />
                 Custom setup for large firms with more than 8 paid roles custom
                 pricing
               </q-card-section>
@@ -195,16 +184,18 @@
         v-if="continueClick === false"
         class="col-md-6 col-sm-12 col-xs-12 bg-white q-pt-lg content-center bg-white"
       >
-        <div class="row">
-          <div class="col-2"></div>
-          <div class="col-8">
+        <div class="column full-height q-px-xl q-pt-xl">
+          <div class="col">
             <q-linear-progress size="10px" :value="progress" />
             <div class="q-mt-xs">Step 1. Create an account</div>
-            <q-form class="q-mt-xl" @submit="onContinue()" ref="orgInfo">
-              <div class="text-h3 text-weight-bolder">Subscribe Now</div>
-              <div class="q-mt-xl"></div>
-              <label class="text-h6 text-weight-bolder">First Name</label>
+            <q-form class="q-mt-lg" @submit="onContinue()" ref="orgInfo">
+              <div class="text-h4 text-weight-bolder">Subscribe Now</div>
+              <div class="q-mt-md"></div>
+              <label class="text-subtitle1 text-weight-bolder"
+                >First Name</label
+              >
               <q-input
+                dense
                 name="firstName"
                 v-model="data.user.contact.fname"
                 color="primary"
@@ -218,11 +209,11 @@
                 ]"
               />
 
-              <div class="q-mt-md"></div>
-              <label class="text-h6 text-weight-bolder">Last Name</label>
+              <label class="text-subtitle1 text-weight-bolder">Last Name</label>
               <q-input
                 v-model="data.user.contact.lname"
                 name="lastName"
+                dense
                 color="primary"
                 placeholder="Enter your Last Name"
                 outlined
@@ -233,11 +224,13 @@
                 ]"
               />
 
-              <div class="q-mt-md"></div>
-              <label class="text-h6 text-weight-bolder">Email Address</label>
+              <label class="text-subtitle1 text-weight-bolder"
+                >Email Address</label
+              >
               <q-input
                 v-model="data.user.email"
                 name="email"
+                dense
                 color="primary"
                 placeholder="Enter Email Address"
                 outlined
@@ -249,10 +242,11 @@
                 ]"
               />
               <span class="text-red text-caption">{{ errorMSG }}</span>
-              <div class="q-mt-md"></div>
-              <label class="text-h6 text-weight-bolder">Password</label>
+
+              <label class="text-subtitle1 text-weight-bolder">Password</label>
               <q-input
                 color="primary"
+                dense
                 class="required full-width"
                 placeholder="Enter your Password"
                 v-model="data.user.password"
@@ -270,11 +264,14 @@
                   />
                 </template>
               </q-input>
-              <div class="q-mt-md"></div>
-              <label class="text-h6 text-weight-bolder">Company Name</label>
+
+              <label class="text-subtitle1 text-weight-bolder"
+                >Company Name</label
+              >
               <q-input
                 v-model="data.company.name"
                 name="businessName"
+                dense
                 color="primary"
                 placeholder="Enter your Company Name"
                 outlined
@@ -285,7 +282,7 @@
                     (val && val.length > 0) || 'Please fill your company name'
                 ]"
               />
-              <div class="q-mt-sm row justify-center">
+              <div class="q-mt-sm row justify-start">
                 <q-checkbox v-model="terms" lazy />
                 <label class="q-mt-sm">I agree to Claimguru's</label>
                 <a class="q-mt-sm q-ml-sm text-deep-orange" href=""
@@ -295,7 +292,7 @@
                 <a class="q-mt-sm text-deep-orange" href="">Privacy Policy</a>
               </div>
 
-              <div class="row justify-center">
+              <div class="row justify-center q-mt-lg">
                 <q-btn
                   label="Continue"
                   no-caps
@@ -306,13 +303,15 @@
                   :disable="!terms"
                 />
               </div>
-              <div class="row justify-center q-mt-sm">
+              <div class="row justify-center q-mt-md">
                 <div class="col-2"></div>
                 <div class="col-8 q-ml-md">
-                  <label class="text-subtitle1 q-ml-xl"
+                  <label class="text-h6 q-ml-xl"
                     >Already have an account?</label
                   >
-                  <a href="/login" class="q-ml-sm text-deep-orange">login</a>
+                  <a href="/login" class="q-ml-sm text-deep-orange text-h6"
+                    >Login</a
+                  >
                 </div>
                 <div class="col-2"></div>
               </div>
@@ -332,18 +331,18 @@
             <div class="q-mt-xs">Step 2. Payment</div>
             <q-form class="q-mt-xl" @submit="onPaymentClick()" ref="orgInfo">
               <div>{{ displayErrors }}</div>
-              <div class="text-h3 text-weight-bolder">Pay with card for</div>
+              <div class="text-h4 text-weight-bolder">Pay with card for</div>
               <div
                 v-if="isPackageSelected.id1 === true"
-                class="text-h3 text-weight-bolder"
+                class="text-h4 text-weight-bolder"
               >
                 Firm Package
               </div>
-              <div v-else class="text-h3 text-weight-bolder">
+              <div v-else class="text-h4 text-weight-bolder">
                 Individual Package
               </div>
               <div class="q-mt-lg"></div>
-              <label class="text-h6 text-weight-bold">Card Number</label>
+              <label class="text-subtitle1 text-weight-bold">Card Number</label>
               <div
                 id="card-number"
                 class="cardInfo f-w-500 text-body1 border-top-left-right q-mt-xs q-mb-lg"
@@ -351,7 +350,9 @@
                 <!-- a Stripe Element will be inserted. -->
               </div>
 
-              <label class="text-h6 text-weight-bold">Name on Card</label>
+              <label class="text-subtitle1 text-weight-bold"
+                >Name on Card</label
+              >
               <q-input
                 borderless
                 class="bg-white cardInfo text-body1 q-mt-xs q-mb-lg"
@@ -369,7 +370,9 @@
 
               <div class="row">
                 <div class="col q-pr-md">
-                  <label class="text-h6 text-weight-bold">Expiry Date</label>
+                  <label class="text-subtitle1 text-weight-bold"
+                    >Expiry Date</label
+                  >
                   <div
                     id="card-expiry"
                     class="cardInfo text-body1 border-bottom-right q-mt-xs"
@@ -378,7 +381,7 @@
                   </div>
                 </div>
                 <div class="col q-pl-xs">
-                  <label class="text-h6 text-weight-bold">CVC</label>
+                  <label class="text-subtitle1 text-weight-bold">CVC</label>
                   <div
                     id="card-cvc"
                     class="cardInfo text-body1 border-bottom-right q-mt-xs"
@@ -390,14 +393,15 @@
 
               <div class="q-mt-md"></div>
               <div class="row">
+                <label class="column text-h5 text-weight-bolder"
+                  >30 days free</label
+                >
+              </div>
+              <div class="row">
                 <div class="col-6">
-                  <label class="column text-h5 text-weight-bolder"
-                    >30 days free</label
-                  >
                   <label class="text-h6 text-weight-bold">After 30 days</label>
                 </div>
                 <div class="col-6">
-                  <div class="column"></div>
                   <div
                     v-if="isPackageSelected.id1 === true"
                     class="text-subtitle1 text-grey-white"
@@ -607,7 +611,7 @@ export default {
       this.setLoading(true);
       var token = '';
       var displayError;
-      await this.createToken().then(function (result) {
+      await this.createToken().then(function(result) {
         if (result.error) {
           displayError = document.getElementById('card-errors');
           if (result.error) {
@@ -627,6 +631,7 @@ export default {
       this.data.stripeToken = token;
       const res = await this.createUserForOrganization(this.data);
       this.setLoading(false);
+      this.$router.push('/setup');
     },
     createToken() {
       // Gather additional customer data we may have collected in our form.
@@ -665,7 +670,7 @@ export default {
   }
 }
 .card-highlighter {
-  background: chocolate;
+  border: 2px solid #ef5926;
 }
 .custom-font {
   font-family: Poppins;
